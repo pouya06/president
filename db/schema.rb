e9df4d4879768f00832a421b579f914fb67df7ff
pouya karimi <pouya06@gmail.com>
2016-01-31 13:26:43 -0800

    forth commit

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160121093228) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "elections", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
